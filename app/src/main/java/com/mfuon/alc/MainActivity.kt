d5f47a47e1477b97a537ac04ae71078bf4fd47c5
Mfuon Leonard <mfolee@gmail.com>
2019-07-15 10:54:55 +0300

    status bar color to transparent

--- app/src/main/java/com/mfuon/alc/MainActivity.kt
+++ app/src/main/java/com/mfuon/alc/MainActivity.kt
@@ -4,12 +4,25 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.WindowManager
+import android.os.Build
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // In Activity's onCreate() for instance
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+
 
         about_alc.setOnClickListener{
             val intent = Intent(this, AboutAlcActivity::class.java)
